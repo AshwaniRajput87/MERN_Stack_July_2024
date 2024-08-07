e25d9d9c7b85b3a27f1102c45ae3c94e0f395c4d
Ashwani Rajput <ashwin.rajput87@gmail.com>
2024-08-07 10:03:37 +0530

    Lec-8 completed the closure and its applications

---
+++ frontend/Complete_JS/Lec-8_Closure_&_its_application_Asynchronous_programming/quiz-4.js
@@ -0,0 +1,14 @@
+function outer() {
+    let arrFn = [];
+    for (var i = 0; i < 3; i++) {
+        arrFn.push(function fn() {
+            i++;
+            console.log(i);
+        })
+    }
+    return arrFn;
+}
+let arrFn = outer();
+arrFn[0]();
+arrFn[1]();
+arrFn[2]();
